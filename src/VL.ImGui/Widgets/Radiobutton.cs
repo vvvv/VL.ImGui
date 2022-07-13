27af2658d9c0b64780753d44983d7c0d03002851
gregsn <gregor@vvvv.org>
2022-07-13 15:21:57 +0200

    Radiobuttons come as a bunch

--- src/VL.ImGui/Widgets/Radiobutton.cs
+++ src/VL.ImGui/Widgets/Radiobutton.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reactive.Disposables;
-using System.Reactive.Subjects;
-using System.Text;
-using VL.Core;
-
-namespace VL.ImGui.Widgets
+﻿namespace VL.ImGui.Widgets
 {
-    [GenerateNode(Category = "ImGui.Widgets")]
-    internal partial class RadioButton : Widget
+    //[GenerateNode(Category = "ImGui.Widgets")]
+    internal partial class RadioButton : ChannelWidget<int>
     {
         public string? Label { get; set; }
 
         public int Index { get; set; }
 
-        public BehaviorSubject<int> Value { get; set; } = new BehaviorSubject<int>(-1);
-
         internal override void Update(Context context)
         {
-            var value = Value.Value;
+            var value = Update();
             if (ImGuiNET.ImGui.RadioButton(Label ?? string.Empty, ref value, Index))
-                if (value == Index) // radio button not only changed but got turned on
-                    Value.OnNext(value);
+                Value = value;
         }
     }
 }
---
+++ src/VL.ImGui/Widgets/Radiobuttons.cs
@@ -0,0 +1,19 @@
+﻿namespace VL.ImGui.Widgets
+{
+    [GenerateNode(Category = "ImGui.Widgets")]
+    internal partial class RadioButtons : ChannelWidget<int>
+    {
+        public IEnumerable<string> Labels { get; set; } = new List<string>();
+
+        internal override void Update(Context context)
+        {
+            var value = Update();
+            int i = 0;
+            foreach (var label in Labels)
+            {
+                if (ImGuiNET.ImGui.RadioButton(label ?? string.Empty, ref value, i++))
+                    Value = value;
+            }
+        }
+    }
+}
