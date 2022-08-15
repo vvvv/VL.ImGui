3104cce636b17b59923e70abc5663d048f5f18b7
antongit <antonworx@gmail.com>
2022-08-15 18:14:29 +0200

    Added more Commands, tried Immediate-Mode Popup - failed.

---
+++ src/VL.ImGui/Commands/SetColorEditPotions.cs
@@ -0,0 +1,18 @@
+﻿using Stride.Core.Mathematics;
+
+namespace VL.ImGui.Widgets
+{
+    [GenerateNode(Category = "ImGui.Commands", GenerateRetained = false)]
+    /// <summary>
+    /// Initialize current options (generally on application startup) if you want to select a default format, picker type, etc.
+    /// </summary>
+    internal partial class SetColorEditOptions : Widget
+    {
+        public ImGuiNET.ImGuiColorEditFlags Flags { private get; set; }
+
+        internal override void Update(Context context)
+        {
+            ImGuiNET.ImGui.SetColorEditOptions(Flags);
+        }
+    }
+}
---
+++ src/VL.ImGui/Windows/PopupCore.cs
@@ -0,0 +1,29 @@
+﻿namespace VL.ImGui.Widgets
+{
+    using ImGui = ImGuiNET.ImGui;
+
+    [GenerateNode(Category = "ImGui.Widgets.Internal", GenerateRetained = false)]
+    public sealed partial class PopupCore : Widget
+    {
+        public Widget? Content { get; set; }
+
+        public string? Label { get; set; }
+
+        public ImGuiNET.ImGuiWindowFlags Flags { private get; set; }
+
+        internal override void Update(Context context)
+        {
+            if (ImGui.BeginPopup(Label ?? string.Empty, Flags))
+            {
+                try
+                {
+                    context?.Update(Content);
+                }
+                finally
+                {
+                    ImGui.EndPopup();
+                }
+            }
+        }
+    }
+}
