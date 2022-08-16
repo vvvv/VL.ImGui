001fce70f29a3170cc231761da1e295b11ec44a5
antongit <antonworx@gmail.com>
2022-08-16 17:55:43 +0200

    Added SetItemWidth

---
+++ src/VL.ImGui/Widgets/SetItemWidth.cs
@@ -0,0 +1,26 @@
+﻿namespace VL.ImGui.Widgets
+{
+    /// <summary>
+    /// Push width of items for common large "item+label" widgets. >0.0f: width in pixels, <0.0f align xx pixels to the right of window (so -FLT_MIN always align width to the right side).
+    /// </summary>
+    [GenerateNode(Category = "ImGui.Widgets", GenerateImmediate = false)]
+    internal partial class SetItemWidth : Widget
+    {
+        public Widget? Content { private get; set; }
+
+        public float Width { private get; set; } = 100f;
+
+        internal override void Update(Context context)
+        {
+            ImGuiNET.ImGui.PushItemWidth(Width);
+            try
+            {
+                context?.Update(Content);
+            }
+            finally
+            {
+                ImGuiNET.ImGui.PopItemWidth();
+            }
+        }
+    }
+}
--- src/VL.ImGui/Widgets/SetTextWrapPosition.cs
+++ src/VL.ImGui/Widgets/SetTextWrapPosition.cs
@@ -1,11 +1,14 @@
 ﻿namespace VL.ImGui.Widgets
 {
-    [GenerateNode(Category = "ImGui.Widgets.Internal", GenerateRetained = false)]
-    internal partial class SetTextWrapPositionCore : Widget
+    /// <summary>
+    /// Push word-wrapping position for Text commands. < 0.0f: no wrapping; 0.0f: wrap to end of window (or column); > 0.0f: wrap at 'wrap_pos_x' position in window local space
+    /// </summary>
+    [GenerateNode(Category = "ImGui.Widgets", GenerateImmediate = false)]
+    internal partial class SetTextWrapPosition : Widget
     {
         public Widget? Content { private get; set; }
 
-        public float Position { private get; set; } = 0f;
+        public float Position { private get; set; } = 100f;
 
         internal override void Update(Context context)
         {
