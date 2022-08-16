0ed7765274e70fa01f1af88d4a2870eaea060250
antongit <antonworx@gmail.com>
2022-08-16 16:29:01 +0200

    Re-added SetTextWrapPosition as an immediate region and retained node. Fixed Text (Multiline). Added howtos. Cosmetics

---
+++ src/VL.ImGui/Widgets/SetTextWrapPositionCore.cs
@@ -0,0 +1,23 @@
+﻿namespace VL.ImGui.Widgets
+{
+    [GenerateNode(Category = "ImGui.Widgets.Internal", GenerateRetained = false)]
+    internal partial class SetTextWrapPositionCore : Widget
+    {
+        public Widget? Content { private get; set; }
+
+        public float Position { private get; set; } = 0f;
+
+        internal override void Update(Context context)
+        {
+            ImGuiNET.ImGui.PushTextWrapPos(Position);
+            try
+            {
+                context?.Update(Content);
+            }
+            finally
+            {
+                ImGuiNET.ImGui.PopTextWrapPos();
+            }
+        }
+    }
+}
