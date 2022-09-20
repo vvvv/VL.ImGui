1a859a5f05edea204ceb6c3e461327dec088ea77
antongit <antonworx@gmail.com>
2022-09-20 08:55:59 +0200

    Update to the TableGetSortSpecs Query. Adds to #29

--- src/VL.ImGui/Widgets/Queries/TableGetSortSpecs.cs
+++ src/VL.ImGui/Widgets/Queries/TableGetSortSpecs.cs
@@ -1,30 +1,29 @@
-﻿// Doesn't work. TableGetSortSpecs causes access violation. I guess because it must be invoked in-between a Begin/End table call.
-// Needs review.
+﻿// Works only in Immediate mode, fails if the Table doesn't have `Sortable` flag.
 
-//using ImGuiNET;
-//using VL.Lib.Collections;
+using ImGuiNET;
+using VL.Lib.Collections;
 
-//namespace VL.ImGui.Widgets
-//{
-//    /// <summary>
-//    /// Get latest sort specs for the table.
-//    /// </summary>
-//    [GenerateNode(Category = "ImGui.Queries")]
-//    internal partial class TableGetSortSpecs : Widget
-//    {
-//        public Spread<TableColumnSortSpecs> Value { get; private set; } = Spread<TableColumnSortSpecs>.Empty;
+namespace VL.ImGui.Widgets
+{
+    /// <summary>
+    /// Get latest sort specs for the table.
+    /// </summary>
+    [GenerateNode(Category = "ImGui.Queries", IsStylable = false)]
+    internal partial class TableGetSortSpecs : Widget
+    {
+        public Spread<TableColumnSortSpecs> Value { get; private set; } = Spread<TableColumnSortSpecs>.Empty;
 
-//        internal override unsafe void UpdateCore(Context context)
-//        {
-//            var specs = ImGuiNET.ImGui.TableGetSortSpecs();
+        internal override unsafe void UpdateCore(Context context)
+        {
+            var specs = ImGuiNET.ImGui.TableGetSortSpecs();
 
-//            var b = Spread.CreateBuilder<TableColumnSortSpecs>(specs.SpecsCount);
-//            var x = new ReadOnlySpan<ImGuiTableColumnSortSpecs>(specs.Specs, specs.SpecsCount);
-//            foreach (var s in x)
-//                b.Add(new TableColumnSortSpecs(s.ColumnUserID, s.ColumnIndex, s.SortOrder, s.SortDirection));
-//            Value = b.ToSpread();
-//        }
-//    }
+            var b = Spread.CreateBuilder<TableColumnSortSpecs>(specs.SpecsCount);
+            var x = new ReadOnlySpan<ImGuiTableColumnSortSpecs>(specs.Specs, specs.SpecsCount);
+            foreach (var s in x)
+                b.Add(new TableColumnSortSpecs(s.ColumnUserID, s.ColumnIndex, s.SortOrder, s.SortDirection));
+            Value = b.ToSpread();
+        }
+    }
 
-//    public record TableColumnSortSpecs(uint ColumnUserID, short ColumnIndex, short SortOrder, ImGuiSortDirection SortDirection);
-//}
+    public record TableColumnSortSpecs(uint ColumnUserID, short ColumnIndex, short SortOrder, ImGuiSortDirection SortDirection);
+}
