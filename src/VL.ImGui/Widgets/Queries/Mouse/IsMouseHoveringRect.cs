94976ad599324439a60937ecf4ee42eecf4a9d69
antongit <antonworx@gmail.com>
2022-10-11 08:42:50 +0200

    IsMouseHoveringRect has now Rectangle input instead of Min/Max. Forwarded `StyleSnapshot` used in GetStyle query.

--- src/VL.ImGui/Widgets/Queries/Mouse/IsMouseHoveringRect.cs
+++ src/VL.ImGui/Widgets/Queries/Mouse/IsMouseHoveringRect.cs
@@ -3,21 +3,21 @@
 namespace VL.ImGui.Widgets
 {
     /// <summary>
-    /// Is mouse hovering given bounding rect (in screen space). clipped by current clipping settings, but disregarding of other consideration of focus/window ordering/popup-block.
+    /// Is mouse hovering given bounding rect (in screen space). Clipped by current clipping settings, but disregarding of other consideration of focus/window ordering/popup-block.
     /// </summary>
     [GenerateNode(Category = "ImGui.Queries")]
     internal partial class IsMouseHoveringRect : Widget
     {
 
-        public Vector2 Min { private get; set; }
-        public Vector2 Max { private get; set; }
+        public RectangleF Rectangle { private get; set; }
+
         public bool Clip { private get; set; }
 
         public bool Value { get; private set; }
 
         internal override void UpdateCore(Context context)
         {
-            Value = ImGuiNET.ImGui.IsMouseHoveringRect(Min.FromHectoToImGui(), Max.FromHectoToImGui(), Clip);
+            Value = ImGuiNET.ImGui.IsMouseHoveringRect(Rectangle.TopLeft.FromHectoToImGui(), Rectangle.BottomRight.FromHectoToImGui(), Clip);
         }
     }
 }
